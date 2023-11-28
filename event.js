c693aa361d07d42c27445e9efc36673b7508dfc8
YuxuanQ8 <113378663+YuxuanQ8@users.noreply.github.com>
2023-11-28 00:11:39 -0600

    Add files via upload

---
+++ event.js
@@ -0,0 +1,9 @@
+function showTable(){
+    var pages = document.getElementById("pages");
+
+    if (pages.style.display == "none"){
+        pages.style.display = "block";
+    }else{
+        pages.style.display = "none";
+    }
+}
